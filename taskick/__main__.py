f4fc5a44cdd346a0a4a0d2cb98b31e8b264be0e5
Atsuya Ide <kappa_deatsu@yahoo.co.jp>
2022-03-20 17:11:41 +0900

    add logging_conf option

--- taskick/__main__.py
+++ taskick/__main__.py
@@ -1,5 +1,7 @@
 import argparse
 import logging
+import logging.config
+import os
 import sys
 
 import yaml
@@ -13,12 +15,32 @@ def main() -> None:
     """_summary_"""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--verbose", "-v", type=str, default="warning", help="")
-    parser.add_argument("--version", "-V", action="store_true", help="")
-    parser.add_argument("--file", "-f", type=str, default=None, help="")
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        action="count",
+        default=0,
+        help='increase the verbosity of messages: "-v" for normal output, "-vv" for more verbose output and "-vvv" for debug',
+    )
+    parser.add_argument("--version", "-V", action="store_true", help="display this application version and exit")
+    parser.add_argument("--file", "-f", type=str, default=None, help="choose task configuration file (YAML)")
+    parser.add_argument(
+        "--logging_config", "-l", type=str, default=None, help="choose logging configuration file (YAML or other)"
+    )
     args = parser.parse_args()
 
-    logging.basicConfig(level=getattr(logging, args.verbose.upper()))
+    # Default logging level: WARNING(30), -vv -> INFO(20)
+    args.verbose = 40 - 10 * args.verbose if args.verbose > 0 else 30
+    logging.basicConfig(level=args.verbose)
+
+    if args.logging_config is not None:
+        file_extention = os.path.splitext(args.logging_config)[-1]
+        if file_extention == ".yaml":
+            with open(args.logging_config, "r") as f:
+                config = yaml.safe_load(f.read())
+                logging.config.dictConfig(config)
+        else:  # .conf, .ini, ...
+            logging.config.fileConfig(args.logging_config)
 
     if args.version:
         print(f"Taskick {__version__}")
